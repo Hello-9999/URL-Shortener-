237ef70810fe8efef2f4087e735bec6f3a176844
Gautam Magar <puskarmagar687@gmail.com>
2024-04-04 21:28:08 +0545

    created Description component

---
+++ src/components/Description.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Description = () => {
+  return (
+    <div>
+      <div className="des_container">
+        <h5 className=" text-center text-2xl font-bold pb-3"> How this work </h5>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+          velit quis soluta, doloremque provident earum nihil quas doloribus
+          minima hic.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Description;
