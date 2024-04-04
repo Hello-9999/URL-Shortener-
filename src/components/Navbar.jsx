074efab95e482e28a4160da8233385f3af96c019
Gautam Magar <puskarmagar687@gmail.com>
2024-04-04 21:27:14 +0545

    Created Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-slate-500 p-3">
+      {" "}
+      <h5 className=""> Logo</h5>
+    </div>
+  );
+};
+
+export default Navbar;
